a259468fa9b4ce7665f5578ecb9b27dfecdb3382
Cas de Rooij <hello@casderooij.nl>
2025-07-01 16:56:07 +0200

    normalize css

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import browserslist from 'browserslist'
 import laravel from 'laravel-vite-plugin'
+import { browserslistToTargets } from 'lightningcss'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    },
+  },
   plugins: [
     laravel({
       input: ['assets/style.css', 'assets/index.js'],
