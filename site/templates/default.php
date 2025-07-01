c70583f3e3bd1a6dd902163c1566386bbe17e01b
Cas de Rooij <hello@casderooij.nl>
2025-07-01 16:45:54 +0200

    add basic styling

--- site/templates/default.php
+++ site/templates/default.php
@@ -5,6 +5,19 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<link
+		rel="preload"
+		href="<?= vite()->asset('assets/fonts/hedvig-letters-sans-regular.woff2') ?>"
+		as="font"
+		type="font/woff2"
+		crossorigin>
+	<link
+		rel="preload"
+		href="<?= vite()->asset('assets/fonts/whois-mono.woff2') ?>"
+		as="font"
+		type="font/woff2"
+		crossorigin>
+
 	<?= vite([
 		'assets/style.css',
 		'assets/index.js'
@@ -14,10 +27,9 @@
 </head>
 
 <body>
-	<a href="/">Home</a>
-	<a href="/test">Test</a>
-
-	<h1><?= $page->title() ?></h1>
+	<header>
+		<a href="/"><?= $page->title() ?></a>
+	</header>
 </body>
 
 </html>
