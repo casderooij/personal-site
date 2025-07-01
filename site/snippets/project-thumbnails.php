be308faa680decb4468fb7c0fd562add78e98c17
Cas de Rooij <hello@casderooij.nl>
2025-07-01 22:58:38 +0200

    style image stack

--- site/snippets/project-thumbnails.php
+++ site/snippets/project-thumbnails.php
@@ -2,25 +2,24 @@
 $projects = site()->find('projects')->children()->listed()->filter(function ($project) {
 	return in_array('experiment', $project->tags()->split(','));
 });
+$index = 0;
 ?>
 
 <?php if ($projects->isNotEmpty()): ?>
-	<section>
-		<h2>Experiments</h2>
-
-		<div>
-			<?php foreach ($projects as $project): ?>
-				<?php if ($image = $project->image()): ?>
-					<?php $thumb = $image->thumb('project'); ?>
-					<img
-						src="<?= $thumb->url() ?>"
-						alt="<?= $image->alt()->or($project->title())->esc() ?>"
-						width="<?= $thumb->width() ?>"
-						height="<?= $thumb->height() ?>"
-						loading="lazy"
-					>
-				<?php endif ?>
-			<?php endforeach ?>
-		</div>
+	<section class="project-thumbnails-stack">
+		<?php foreach ($projects as $project): ?>
+			<?php if ($image = $project->image()): ?>
+				<?php $thumb = $image->thumb('project'); ?>
+				<img
+					src="<?= $thumb->url() ?>"
+					alt="<?= $image->alt()->or($project->title())->esc() ?>"
+					width="<?= $thumb->width() ?>"
+					height="<?= $thumb->height() ?>"
+					style="--stack-i: <?= $index ?>"
+					loading="lazy"
+				>
+			<?php endif ?>
+			<?php $index++; ?>
+		<?php endforeach ?>
 	</section>
 <?php endif ?>
