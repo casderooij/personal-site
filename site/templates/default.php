6e51943048070f12521a7dd9e84748704e6e44fd
Cas de Rooij <hello@casderooij.nl>
2025-07-01 13:24:08 +0200

    add vite

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,4 +1,23 @@
-<a href="/">Home</a>
-<a href="/test">Test</a>
+<!DOCTYPE html>
+<html lang="en">
 
-<h1><?= $page->title() ?></h1>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<?= vite([
+		'assets/style.css',
+		'assets/index.js'
+	]) ?>
+
+	<title>Cas de Rooij</title>
+</head>
+
+<body>
+	<a href="/">Home</a>
+	<a href="/test">Test</a>
+
+	<h1><?= $page->title() ?></h1>
+</body>
+
+</html>
