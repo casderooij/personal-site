a571f5b792968189cf99b4b9dc847b94c6472a19
Cas de Rooij <hello@casderooij.nl>
2025-07-02 15:44:27 +0200

    manual upload poster image

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,5 @@
 <?php
 
-use SitePlugin\PosterGenerator;
-
 return [
 	'thumbs' => [
 		'presets' => [
@@ -10,18 +8,5 @@ return [
 				'format' => 'webp'
 			]
 		]
-	],
-	'hooks' => [
-		'file.create:after' => function ($file) {
-			// Check if the uploaded file is a video
-			if ($file->type() === 'video') {
-				// Generate poster using the new class
-				PosterGenerator::generatePoster($file, $file->parent());
-			}
-		},
-		'file.delete:after' => function ($file) {
-			// Delete poster using the new class
-			PosterGenerator::deletePoster($file);
-		}
 	]
 ];
