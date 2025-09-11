da6da55264939aa82d3e877af150282c057308f8
Cas de Rooij <hello@casderooij.nl>
2025-09-11 11:41:51 +0200

    Reset css and add fonts

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -12,6 +12,20 @@
 		type="font/woff2"
 		crossorigin>
 
+	<link
+		rel="preload"
+		href="<?= vite()->asset('assets/fonts/whois-mono.woff2') ?>"
+		as="font"
+		type="font/woff2"
+		crossorigin>
+
+	<link
+		rel="preload"
+		href="<?= vite()->asset('assets/fonts/hedvig-letters-sans.woff2') ?>"
+		as="font"
+		type="font/woff2"
+		crossorigin>
+
 	<link rel="icon" href="<?= url('favicon.png') ?>" type="image/png">
 
 	<?= vite([
