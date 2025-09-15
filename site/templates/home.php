9406dedd051fe6f15cd5e98b08af0c1d255ced84
Cas de Rooij <hello@casderooij.nl>
2025-09-15 08:54:50 +0200

    add intro text and video

--- site/templates/home.php
+++ site/templates/home.php
@@ -17,10 +17,6 @@
         <h1>creative web developer</h1>
     </div>
 
-
-
-
-
     <div id="sphere-container" class="sphere-container" data-videos="<?= htmlspecialchars($encodedIntroVideos, ENT_QUOTES, 'UTF-8') ?>">
         <div id="selected-video-container" class="selected-video-container">
             <div id="selected-video-title" class="selected-video-title"></div>
@@ -29,47 +25,29 @@
     <div id="sphere-videos-container" class="sphere-videos-container"></div>
 </header>
 
-<button id="scroll-down-to-main-button" class="scroll-down-button">Scroll down</button>
+<button id="scroll-down-to-main-button" class="scroll-down-button">Scroll down for info</button>
 
 <main>
     <div class="inner">
-        <section class="intro-text">
-            <?= $page->introText() ?>
+        <section class="intro-section">
+            <video
+                class="intro-video"
+                src="/assets/one-minute-capture.mp4"
+                autoplay playsinline muted loop></video>
+
+            <div class="intro-text">
+                <?= $page->introText() ?>
+            </div>
         </section>
     </div>
 </main>
 
-<footer></footer>
-
-<!-- <main>
-    <section class="intro-section">
-        <div class="intro-content">
-            <p>
-                I am a web developer working at the intersection of user experience,
-                design and technology. Since 2018, I've been building solutions such
-                as booking flows, data dashboards, micro sites, 3D animations, and
-                much more.
-            </p>
-
-            <p>
-                I gain knowledge from working on professional projects as well as
-                from personal experiments. The insights and experiences I gain from
-                personal projects often flow back into my client-based work.
-            </p>
-
-            <p>
-                I find the web a beautiful, accessible canvas for telling stories,
-                giving insights and providing tools. Lots of things can be build for
-                the web, and I'm here to do just that!
-            </p>
-        </div>
-
-        <div class="intro-links">
-            <?php snippet('external-link', ['label' => 'Github', 'url' => 'https://github.com/casderooij']) ?>
-            <?php snippet('external-link', ['label' => 'Bluesky', 'url' => 'https://bsky.app/profile/casderooij.nl']) ?>
-            <?php snippet('external-link', ['label' => 'LinkedIn', 'url' => 'https://www.linkedin.com/in/cas-de-rooij-86374861/']) ?>
-        </div>
-    </section>
-</main> -->
+<footer>
+    <div class="links-container">
+        <?php snippet('external-link', ['label' => 'Github', 'url' => 'https://github.com/casderooij']) ?>
+        <?php snippet('external-link', ['label' => 'Bluesky', 'url' => 'https://bsky.app/profile/casderooij.nl']) ?>
+        <?php snippet('external-link', ['label' => 'LinkedIn', 'url' => 'https://www.linkedin.com/in/cas-de-rooij-86374861/']) ?>
+    </div>
+</footer>
 
 <?php snippet('footer') ?>
