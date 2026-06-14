3f6ed81c4b43b06859cb14b0c972f778d70e5957
Cas de Rooij <hello@casderooij.nl>
2026-06-15 00:31:02 +0200

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -16,9 +16,9 @@ snippet('head');
     <a href="<?= site()->url() ?>">Cas de Rooij</a>
 </header>
 
-<nav class="flex items-center gap-2 mb-16 sticky top-4">
-    <a href="<?= $prevLink ?>" class="text-xs bg-[#dcdcff] px-4 py-0.5 rounded-full hover:bg-white transition duration-300 ease-in-out">&larr; <?= $prevMonth ?></a>
-    <a href="<?= $nextLink ?>" class="text-xs bg-[#dcdcff] px-4 py-0.5 rounded-full hover:bg-white transition duration-300 ease-in-out"><?= $nextMonth ?> &rarr;</a>
+<nav class="flex items-center gap-2 mb-16 sticky top-4 z-10">
+    <a href="<?= $prevLink ?>" class="text-xs bg-[#dcdcff] px-4 py-0.5 sm:py-1 rounded-full hover:bg-white transition duration-300 ease-in-out">&larr; <?= $prevMonth ?></a>
+    <a href="<?= $nextLink ?>" class="text-xs bg-[#dcdcff] px-4 py-0.5 sm:py-1 rounded-full hover:bg-white transition duration-300 ease-in-out"><?= $nextMonth ?> &rarr;</a>
 </nav>
 
 <span class="mb-2 inline-block"><?= $currentMonth ?></span>
