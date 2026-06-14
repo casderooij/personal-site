325f4edb849209029ca397603a695baf3e990050
Cas de Rooij <hello@casderooij.nl>
2026-06-15 00:00:04 +0200

    Add footer top margin

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
 <?php $externalLinks = site()->footerLinks()->toStructure(); ?>
 
-<footer>
+<footer class="mt-16">
     <?php foreach ($externalLinks as $link): ?>
         <a class="underline text-[blue]" href="<?= $link->url() ?>"><?= $link->label() ?></a>
     <?php endforeach  ?>
