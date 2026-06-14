f85267b19c042aa4124f24e9cc8827368f7b4fde
Cas de Rooij <hello@casderooij.nl>
2026-06-15 00:32:03 +0200

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -17,8 +17,8 @@ snippet('head');
 </header>
 
 <nav class="flex items-center gap-2 mb-16 sticky top-4 z-10">
-    <a href="<?= $prevLink ?>" class="text-xs bg-[#dcdcff] px-4 py-0.5 sm:py-1 rounded-full hover:bg-white transition duration-300 ease-in-out">&larr; <?= $prevMonth ?></a>
-    <a href="<?= $nextLink ?>" class="text-xs bg-[#dcdcff] px-4 py-0.5 sm:py-1 rounded-full hover:bg-white transition duration-300 ease-in-out"><?= $nextMonth ?> &rarr;</a>
+    <a href="<?= $prevLink ?>" class="text-xs bg-[#dcdcff] px-4 py-1 sm:py-0.5 rounded-full hover:bg-white transition duration-300 ease-in-out">&larr; <?= $prevMonth ?></a>
+    <a href="<?= $nextLink ?>" class="text-xs bg-[#dcdcff] px-4 py-1 sm:py-0.5 rounded-full hover:bg-white transition duration-300 ease-in-out"><?= $nextMonth ?> &rarr;</a>
 </nav>
 
 <span class="mb-2 inline-block"><?= $currentMonth ?></span>
